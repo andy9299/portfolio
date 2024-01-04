b7a990c85d2d1e0def5f4fb299fa966e217feb0d
andy9299 <andy.tu998@gmail.com>
2024-01-04 17:29:56 -0500

    Basic Homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1 className='text-4xl'> Test</h1>
-<a className="inline-flex flex-wrap
-items-center justify-center text-center
-cursor-pointer select-none border
-transition duration-200 ease-in-out
-flex-shrink-0 font-semibold py-2 px-4
-rounded-md disabled:opacity-80
-disabled:pointer-events-none
-focus:outline-none focus:ring-2
-focus:ring-opacity-50 hover:no-underline
-bg-blue-600 text-white
-hover:bg-blue-700
-py-3 px-5 text-lg">Button</a>
-<a className="btn btn-primary btn-lg">Button</a>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='flex h-screen w-full items-center justify-center'>
+      <div className='grid grid-cols-2 grid-rows-1 divide-x-2 divide-solid'>
+        <div className=''>
+          <h1 className='text-[10rem] pr-8 '>At</h1>
+        </div>
+        <div className='pl-8 flex flex-col justify-center'>
+          <a className='text-2xl hover:underline'>Projects</a>
+          <a className='text-2xl hover:underline'>About Me</a>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className='absolute bottom-0 flex h-20 gap-10'>
+        <a href="https://github.com/andy9299" className='hover:underline'>GitHub</a>
+        <a href="https://www.linkedin.com/in/andy-tu-58a797149/" className='hover:underline'>LinkedIn</a>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+    },
+  },
+  daisyui: {
+    themes: ["synthwave"],
   },
   plugins: [require("daisyui")],
 }
